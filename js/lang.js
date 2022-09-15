b15a9439eb931083174217bae3e784d3576af11f
kovalchuk-mykhailo <mkovalch@softeserveinc.com>
2022-09-15 14:48:04 +0300

    npm init, make 'lang.js' FIFO

--- js/lang.js
+++ js/lang.js
@@ -1,68 +1,70 @@
+// import { initMenu } from "./menu";
 import ua from "../locals/ua.json" assert { type: "json" };
 import en from "../locals/en.json" assert { type: "json" };
-// import { initMenu } from "./menu";
 
-const initMenu = () => {
-  const burgerBtnRef = document.querySelector(".burger-btn");
-  const mobileMenuRef = document.querySelector(".menu");
+(() => {
+  const initMenu = () => {
+    const burgerBtnRef = document.querySelector(".burger-btn");
+    const mobileMenuRef = document.querySelector(".menu");
 
-  document.addEventListener("click", (e) => {
-    if (
-      e.target.classList.contains("nav-link") ||
-      e.target.classList.contains("support-btn")
-    ) {
-      burgerBtnRef.classList.toggle("open");
-      mobileMenuRef.classList.toggle("is-hidden");
-    }
-  });
-
-  burgerBtnRef.addEventListener("click", () => {
-    burgerBtnRef.classList.toggle("open");
-    mobileMenuRef.classList.toggle("is-hidden");
-  });
-};
-
-let currentLang = ua;
-console.log("Init script");
-
-window.addEventListener("load", (event) => {
-  initMenu();
-
-  console.log("resources ARE LOADED!");
-
-  const switchLang = document.getElementById("language-switcher");
-  const langText = document.querySelectorAll("[data-lang]");
-
-  alert(`switchLang: ${switchLang}; langText(q-ty): ${langText.length}`);
-
-  const translate = (lang) => {
-    localStorage.setItem("lang", lang);
-    const source = lang === "ua" ? ua : en;
-    langText.forEach((el) => {
-      el.innerText = source[el.dataset.lang];
+    document.addEventListener("click", (e) => {
+      if (
+        e.target.classList.contains("nav-link") ||
+        e.target.classList.contains("support-btn")
+      ) {
+        burgerBtnRef.classList.toggle("open");
+        mobileMenuRef.classList.toggle("is-hidden");
+      }
     });
 
-    console.log(source);
-
-    currentLang = lang;
+    burgerBtnRef.addEventListener("click", () => {
+      burgerBtnRef.classList.toggle("open");
+      mobileMenuRef.classList.toggle("is-hidden");
+    });
   };
 
-  switchLang.addEventListener("change", (e) => {
-    alert(
-      `Click.target: ${e.target.value} ;
+  let currentLang = ua;
+  console.log("Init script");
+
+  window.addEventListener("load", (event) => {
+    initMenu();
+
+    console.log("resources ARE LOADED!");
+
+    const switchLang = document.getElementById("language-switcher");
+    const langText = document.querySelectorAll("[data-lang]");
+
+    alert(`switchLang: ${switchLang}; langText(q-ty): ${langText.length}`);
+
+    const translate = (lang) => {
+      localStorage.setItem("lang", lang);
+      const source = lang === "ua" ? ua : en;
+      langText.forEach((el) => {
+        el.innerText = source[el.dataset.lang];
+      });
+
+      console.log(source);
+
+      currentLang = lang;
+    };
+
+    switchLang.addEventListener("change", (e) => {
+      alert(
+        `Click.target: ${e.target.value} ;
        Click.currentTarget: ${e?.currentTarget?.value}`
-    );
+      );
 
-    console.log();
-    translate(e.target.value);
+      console.log();
+      translate(e.target.value);
+    });
+
+    window.addEventListener("load", () => {
+      const lang = localStorage.getItem("lang");
+
+      if (lang) {
+        translate(lang);
+        switchLang.value = lang;
+      }
+    });
   });
-
-  window.addEventListener("load", () => {
-    const lang = localStorage.getItem("lang");
-
-    if (lang) {
-      translate(lang);
-      switchLang.value = lang;
-    }
-  });
-});
+})();
